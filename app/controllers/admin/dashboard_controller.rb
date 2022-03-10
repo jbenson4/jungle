4f526edcce3f98a8ff752eee7fc836e527cc822d
Jordan <jordan.benson4@gmail.com>
2022-03-09 22:44:35 -0800

    Pass product and category count to index.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: Rails.application.secrets.admin_username, password: Rails.application.secrets.admin_password
 
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
