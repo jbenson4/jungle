53ac436496b69f745ef450ca27248bd0e10d80a8
Jordan <jordan.benson4@gmail.com>
2022-03-15 10:13:13 -0700

    Add more spec examples.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'creates a valid product' do
-    a_product = Product.create("name" => "Skateboard", "price" => 120, "quantity" => 10, "category" => @a_category)
-    expect(a_product).to be_valid
+  it 'creates a valid user' do
+    a_user = User.create({:first_name => "Bob", :last_name => "Smith", :email => "user@example.com", :password => "password", :password_confirmation => "password"})    
+    expect(a_user).to be_valid
+  end
+  it 'rejects incorrect password confirmation' do
+    a_user = User.create({:first_name => "Bob", :last_name => "Smith", :email => "user@example.com", :password => "password", :password_confirmation => "pasword"})    
+    expect(a_user).to be_valid
+  end
+  it 'requires an email to be valid' do
+    a_user = User.create({:first_name => "Bob", :last_name => "Smith", :email => nil, :password => "password", :password_confirmation => "password"})    
+    expect(a_user).to be_valid
+  end
+  it 'requires a first name to be valid' do
+    a_user = User.create({:first_name => "Bob", :last_name => "Smith", :email => "user@example.com", :password => "password", :password_confirmation => "password"})    
+    expect(a_user).to be_valid
+  end
+  it 'requires a last name to be valid' do
+    a_user = User.create({:first_name => "Bob", :last_name => "Smith", :email => "user@example.com", :password => "password", :password_confirmation => "password"})    
+    expect(a_user).to be_valid
+  end
+  it 'ensures emails are unique' do
+    user1 = User.create({:first_name => "Bob", :last_name => "Smith", :email => "user@example.com", :password => "password", :password_confirmation => "password"})    
+    user2 = User.create({:first_name => "Bob", :last_name => "Smith", :email => "USER@EXAMPLE.com", :password => "password", :password_confirmation => "password"})    
+    expect(user2).to_not exist
+  end
+  it 'should have a password minimum length' do
+    a_user = User.create({:first_name => "Bob", :last_name => "Smith", :email => "user@example.com", :password => "p", :password_confirmation => "p"})    
+    expect(a_user).to_not exist
   end
 end
