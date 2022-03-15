0ceb499157954168750a52446d3359a504fb76c4
Jordan <jordan.benson4@gmail.com>
2022-03-14 20:01:21 -0700

    Create spec examples for products.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,9 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+    before(:each) do
+      @a_category = Category.create({:name => 'Sports'})
+    end
+    
+    it 'creates a valid product' do
+      a_product = Product.create("name" => "Skateboard", "price" => 120, "quantity" => 10, "category" => @a_category)
+      expect(a_product).to be_valid
+    end
     it 'validates name' do
-      product = Product.new(name => 'Food')
-      expect(product['name']).to be present
+      a_product = Product.new("name" => nil, "price" => 120, "quantity" => 10, "category" => @a_category)
+      expect(a_product).to_not be_valid
+    end
+    it 'validates price' do
+      a_product = Product.new("name" => "Skateboard", "price" => nil, "quantity" => 10, "category" => @a_category)
+      expect(a_product).to_not be_valid
+    end
+    it 'validates quantity' do
+      a_product = Product.new("name" => "Skateboard", "price" => 120, "quantity" => nil, "category" => @a_category)
+      expect(a_product).to_not be_valid
+    end
+    it 'validates category' do
+      a_product = Product.new("name" => "Skateboard", "price" => 120, "quantity" => 10, "category" => nil)
+      expect(a_product).to_not be_valid
     end
   end
 end
