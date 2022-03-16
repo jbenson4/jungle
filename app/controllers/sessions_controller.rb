7fd00c276e97c6637327bed41ec47f256e8c91f2
Jordan <jordan.benson4@gmail.com>
2022-03-15 19:15:27 -0700

    Change create path to use User model authentication method.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:session][:email])
-    # puts 'user' user
-    if user && user.authenticate(params[:session][:password])
+    user = User.authenticate_with_credentials(params[:email], params[:password])
+    if User.authenticate_with_credentials(params[:email].strip, params[:password])
       session[:user_id] = user.id
       redirect_to '/'
     else
