51d4d4ac2de01f9d623416191aead227d0bac2fe
Jordan <jordan.benson4@gmail.com>
2022-03-15 19:13:32 -0700

    Add validations and authenticate method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ActiveRecord::Base
   has_secure_password
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :password, length: { minimum: 3 }
+  validates :email, uniqueness: { case_sensitive: false}
+  
+  def self.authenticate_with_credentials(email, password)
+    stripped_email = email.downcase.strip
+    user = User.find_by_email(stripped_email)
+    if user && user.authenticate(password)
+      user
+    else
+      false
+    end
+  end
 end
